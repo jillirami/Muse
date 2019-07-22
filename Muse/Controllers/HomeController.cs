dc98b9b8e2d29ac0ebf0e3d688129dbf870d2be4
Jillianne Ramirez <36245782+jillirami@users.noreply.github.com>
2019-07-22 12:17:11 -0700

    restarted program to remove bugs introduced from visual studio mac environment

---
+++ Muse/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Muse_Redo.Models;
+
+namespace Muse_Redo.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Homepage()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
--- README.md
+++
@@ -1,3 +0,0 @@
-# Muse
-
-### A new kind of lifestyle application to remove influences of external validation and promote self reflection
